42b69abaa98867e11d2404b045eaa459247b39ee
Fernando Torres <tp.fernando@gmail.com>
2021-07-22 13:01:50 -0300

    Deploy ;)

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">App exchange v1.0.0</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Proyecto para obtener las cotizaciones de las críptomonedas más
+      importantes a traves de una API.
+      <br />Es un proyecto que emplea Vuejs.
+    </p>
+    <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline"
+      >Volver a la página de innicio.
+    </router-link>
+  </div>
+</template>
